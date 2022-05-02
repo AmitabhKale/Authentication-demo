fac4ab297af11d3b74450a7ca9be920d958e6809
Amitabh Kale <amitabhkale@gmail.com>
2022-05-02 15:14:11 +0530

    Level 4 - Salting and hashing

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const asyncHandler = require('express-async-handler')
 const router = express.Router();
 const User = require('../model/userModel')
-const md5 = require('md5')
+const bcrypt = require('bcrypt')
+
+const saltRounds = 10;
 
 
 
@@ -10,6 +12,7 @@ const md5 = require('md5')
 // @route    /api/users
 // @access   Public
 router.post('/register', asyncHandler( async (req,res) => {
+
     const {name, email, password} = req.body;
     if(!name || !password || !email ){
         res.status(400)
@@ -23,14 +26,14 @@ router.post('/register', asyncHandler( async (req,res) => {
         res.status(400)
         throw new Error('User already exists')
     }
+    
 
-    // create user
-    const user = await User.create({
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+    const user =  User.create({
         name,
         email,
-        password : md5(password)
+        password : hash
     })
-
     if(user){
         res.status(201).json({
             name: user.name,
@@ -41,6 +44,7 @@ router.post('/register', asyncHandler( async (req,res) => {
     res.status(400)
     throw new Error('Invalid User data')
 }
+    })
 }))
 
 
@@ -53,11 +57,15 @@ router.post('/login',asyncHandler( async (req,res) => {
 
     const user = await User.findOne({email : email})
 
-    if(user && md5(password) === user.password){
-        res.status(200).json({
-            name: user.name,
-            email : user.email,
-            message: 'Logged In !'
+    if(user){
+        bcrypt.compare(password, user.password, (err,result) => {
+            if(result === true){
+                res.status(200).json({
+                    name: user.name,
+                    email : user.email,
+                    message: 'Logged In !'
+                })
+            }
         })
     }else{
         res.status(401)
